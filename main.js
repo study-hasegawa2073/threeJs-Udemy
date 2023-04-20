2bd822e8847b89546851f95f5009baec7e21f6d1
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-20 12:38:41 +0900

    fix: path

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import style from './style.css';
 
 let scene, camera, renderer, pointLight, controls;
 
