fae82994035d2dcd329bfb3078c61aadb14c9efc
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-20 12:33:25 +0900

    Three.jsに触れてみた(地球)

--- main.js
+++ main.js
@@ -1,47 +1,72 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import style from './style.css';
 
-let scene, camera, renderer, pointLight;
+let scene, camera, renderer, pointLight, controls;
 
-// シーンを作成
-scene = new THREE.Scene();
+window.addEventListener('load', init);
 
-// カメラを作成
-camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(0, 0, 500);
+function init() {
+  // シーンを作成
+  scene = new THREE.Scene();
 
-// レンダラーを作成
-renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-renderer.render(scene, camera);
+  // カメラを作成
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(0, 0, 500);
 
-// ジオメトリを作成
-const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
-// マテリアルを作成
-const ballMaterial = new THREE.MeshPhysicalMaterial();
-// メッシュ化
-const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-scene.add(ballMesh);
+  // レンダラーを作成
+  renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
+  renderer.render(scene, camera);
 
-// 並行光源を作成
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-directionalLight.position.set(1, 1, 1);
-scene.add(directionalLight);
+  // テクスチャを作成
+  let texture = new THREE.TextureLoader().load('./textures/earth.jpg');
 
-// ポイント光源を作成
-pointLight = new THREE.PointLight(0xffffff, 2);
-pointLight.position.set(-200, -200, -200);
-scene.add(pointLight);
+  // ジオメトリを作成
+  const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
+  // マテリアルを作成
+  const ballMaterial = new THREE.MeshPhysicalMaterial({ map: texture });
+  // メッシュ化
+  const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+  scene.add(ballMesh);
 
-// ポイント光源の位置を特定するヘルパーを作成
-let pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
-scene.add(pointLightHelper);
+  // 並行光源を作成
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
+
+  // ポイント光源を作成
+  pointLight = new THREE.PointLight(0xffffff, 2);
+  pointLight.position.set(-200, -200, -200);
+  scene.add(pointLight);
+
+  // ポイント光源の位置を特定するヘルパーを作成
+  let pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
+  scene.add(pointLightHelper);
+
+  // マウス操作ができるようにする
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  window.addEventListener('resize', onWindowResize, false);
+
+  animate();
+}
+
+// ブラウザのリサイズに対応
+function onWindowResize() {
+  // レンダラーのサイズを随時更新
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // カメラのアスペクト比を正す
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
 
 // ポイント光源を球の周りを巡回させる
 function animate() {
@@ -54,4 +79,3 @@ function animate() {
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
-animate();
